8b5af7b52a384bc8046c3b43c3a6917e0b3b1d0c
Nutchapon Hanouypornlert <nutcha.h@Nutchapons-MacBook-Pro.local>
2021-08-03 18:33:09 +0700

    change defualt auth setting to token and add permission class on function view

--- first_app/api/views.py
+++ first_app/api/views.py
@@ -1,8 +1,9 @@
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
 @api_view()
-@permission_classes([AllowAny])
+@permission_classes([IsAuthenticated])
 def first_function(request):
+    params = request.query_params
     return Response({ 'message': 'hello' })
