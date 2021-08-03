e9081c2fa6017ec2138eab2f740d3559863a1944
Nutchapon Hanouypornlert <nutcha.h@Nutchapons-MacBook-Pro.local>
2021-08-03 17:03:06 +0700

    add model CarSpecs

--- first_app/models.py
+++ first_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class CarSpecs(models.Model):
+    car_brand = models.CharField(max_length=100)
+    car_model = models.CharField(max_length=50)
+    production_year = models.CharField(max_length=20)
+    car_body = models.CharField(max_length=100)
+    engine_type = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.car_brand
+        
